a6099093abe3318f503b5a64917b813d7ddbcfe4
John Owen Danuarta <john.owen.d19@gmail.com>
2022-11-09 10:58:02 +0700

    Adding new tglhariini component

---
+++ src/app/tglhariini/tglhariini.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tglhariini',
+  templateUrl: './tglhariini.component.html',
+  styleUrls: ['./tglhariini.component.scss'],
+})
+export class TglhariiniComponent implements OnInit {
+  constructor() { }
+  
+  ngOnInit() { }
+
+  tglstring() {
+    let d = new Date();
+    let tahun = d.getFullYear();
+    let bulan = d.getMonth() + 1;
+    let tgl = d.getDate();
+    let hr = d.getDay();
+    let hari: string = '';
+    if (hr == 0) hari = 'Minggu';
+    if (hr == 1) hari = 'Senin';
+    if (hr == 2) hari = 'Selasa';
+    if (hr == 3) hari = 'Rabu';
+    if (hr == 4) hari = 'Kamis';
+    if (hr == 5) hari = 'Jumat';
+    if (hr == 6) hari = 'Sabtu';
+    let tgl_str = hari + ", " + tgl + "-" + bulan + "-" + tahun;
+    return tgl_str;
+  }
+  tanggal = this.tglstring()
+}
