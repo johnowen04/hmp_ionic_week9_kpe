0d8baae5438b6c08bf189463ec779853f7a1a9ea
John Owen Danuarta <john.owen.d19@gmail.com>
2022-11-09 11:55:29 +0700

    Improving product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {TglhariiniComponent} from './tglhariini/tglhariini.component';
 import { ProductComponent } from './product/product.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, TglhariiniComponent, ProductComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -11,4 +11,40 @@ export class ProductComponent implements OnInit {
 
   ngOnInit() {}
 
+  productName: string = "Gadget Baru"
+  productPrice: number = 8000000
+  productImg: string = "https://placeimg.com/200/200/tech"
+  nettPrice: number = this.productPrice
+  kodeVoucher: string
+
+  discount(): number {
+    return this.productPrice * 0.2
+  }
+
+  applyDiscount(): void {
+    this.nettPrice -= this.discount()
+    this.btnDisable = true
+  }
+
+  checkVoucher(event: Event) {
+    var element = event.target as HTMLInputElement
+    this.btnDisable = element.value == "007" ? false : true
+    
+    // if (element.value == "007") {
+    //   this.btnDisable = false
+    // } else {
+    //   this.btnDisable = true 
+    // }
+
+    // this.kodeVoucher = this.btnDisable ? "" : element.value
+
+    // if (!this.btnDisable) {
+    //   this.kodeVoucher = element.value
+    // } else {
+    //   this.kodeVoucher = ""
+    // }
+  }
+
+  btnDisable: boolean = true
+  btnColor = "danger"
 }
